9c93311174dc58cfabe2410baf16e8cc2b3893fa
jonascolas <jonas@fusemachines.com>
2022-03-10 00:12:38 -0300

    add new page with input

---
+++ lib/ui/add_task_bar.dart
@@ -0,0 +1,55 @@
+import 'package:avis/ui/theme.dart';
+import 'package:avis/ui/widgets/input_field.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AddTaskPage extends StatelessWidget {
+  const AddTaskPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: context.theme.backgroundColor,
+      appBar: _appBar(context),
+      body: _taskBody(),
+    );
+  }
+
+  _appBar(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: context.theme.backgroundColor,
+      leading: GestureDetector(
+        onTap: () {
+          Get.back();
+        },
+        child: Icon(
+          Icons.arrow_back_ios,
+          size: 20,
+          color: Get.isDarkMode ? Colors.white : Colors.black,
+        ),
+      ),
+      actions: const [
+        CircleAvatar(
+          backgroundImage: AssetImage("images/profile.png"),
+        ),
+        SizedBox(width: 20),
+      ],
+    );
+  }
+
+  _taskBody() {
+    return Container(
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Add Task", style: headingStyle),
+            MyInputField(title: "Title", hint: "Enter your title"),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,7 +1,14 @@
 import 'package:avis/services/notification_services.dart';
 import 'package:avis/services/theme_services.dart';
+import 'package:avis/ui/add_task_bar.dart';
+import 'package:avis/ui/theme.dart';
+import 'package:avis/ui/widgets/button.dart';
+import 'package:date_picker_timeline/date_picker_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+// import 'package:flutter/cupertino.dart';
 
 class Home extends StatefulWidget {
   Home({Key? key}) : super(key: key);
@@ -11,6 +18,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  DateTime _selectedDate = DateTime.now();
   var notifyHelper;
 
   @override
@@ -26,11 +34,71 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: _appBar(),
       body: Column(
-        children: const [
-          Text(
-            "Theme Data",
-            style: TextStyle(fontSize: 30),
+        children: [
+          _addTaskBar(),
+          _addDateBar(),
+        ],
+      ),
+    );
+  }
+
+  _addDateBar() {
+    return Container(
+      margin: const EdgeInsets.only(top: 20, left: 20),
+      child: DatePicker(
+        DateTime.now(),
+        height: 100,
+        width: 80,
+        initialSelectedDate: DateTime.now(),
+        selectionColor: primaryClr,
+        selectedTextColor: Colors.white,
+        dateTextStyle: GoogleFonts.lato(
+          textStyle: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            color: Colors.grey,
           ),
+        ),
+        dayTextStyle: GoogleFonts.lato(
+          textStyle: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+            color: Colors.grey,
+          ),
+        ),
+        monthTextStyle: GoogleFonts.lato(
+          textStyle: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w600,
+            color: Colors.grey,
+          ),
+        ),
+        onDateChange: (date) {
+          _selectedDate = date;
+        },
+      ),
+    );
+  }
+
+  _addTaskBar() {
+    return Container(
+      margin: const EdgeInsets.only(left: 20, top: 10, right: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  DateFormat.yMMMMd().format(DateTime.now()),
+                  style: subHeadingStyle,
+                ),
+                Text("Today", style: headingStyle),
+              ],
+            ),
+          ),
+          MyButton(label: "+ Add Task", onTap: () => Get.to(AddTaskPage()))
         ],
       ),
     );
@@ -38,9 +106,10 @@ class _HomeState extends State<Home> {
 
   _appBar() {
     return AppBar(
+      elevation: 0,
+      backgroundColor: context.theme.backgroundColor,
       leading: GestureDetector(
         onTap: () {
-          // print('tapped');
           ThemeService().switchTheme();
           notifyHelper.displayNotification(
             title: "Title: Theme Changed",
@@ -48,10 +117,16 @@ class _HomeState extends State<Home> {
           );
           notifyHelper.scheduledNotification();
         },
-        child: const Icon(Icons.nightlight_round, size: 20),
+        child: Icon(
+          Get.isDarkMode ? Icons.wb_sunny_outlined : Icons.nightlight_round,
+          size: 20,
+          color: Get.isDarkMode ? Colors.white : Colors.black,
+        ),
       ),
       actions: const [
-        Icon(Icons.person, size: 20),
+        CircleAvatar(
+          backgroundImage: AssetImage("images/profile.png"),
+        ),
         SizedBox(width: 20),
       ],
     );
